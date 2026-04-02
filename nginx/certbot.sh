b150e70aa3bef023d755448837699d0609ddc119
Alfonso da Silva <adasilva@tetrao.eu>
2026-04-02 17:58:12 +0200

    Remove nginx

--- nginx/certbot.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-
-docker run -it --rm --name certbot \
-        -p 80:80 -p 443:443 \
-        -v "$SCRIPTPATH/etc_letsencrypt:/etc/letsencrypt" \
-        certbot/certbot $@
